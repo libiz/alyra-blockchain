81a1172234dadf4964fe4e6b738f40906387024e
libiz <ridy.nkayilu@gmail.com>
2019-04-18 16:41:05 +0200

    Exercice 1.2.1

---
+++ exercices/chapitre1/exo/1.2.1/factorielle.js
@@ -0,0 +1,14 @@
+function factorielle(nbr){
+
+    if (nbr > 1 ){
+
+        return nbr * factorielle(nbr - 1);
+
+    }else{
+        return 1 ;
+    }
+
+}
+
+let nbr = process.argv[2];
+console.log(factorielle(nbr))
